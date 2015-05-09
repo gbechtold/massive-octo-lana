966adad871fe1ca9f6d9d2cdd1dc9ed05f39578b
Guntram Bechtold <gb@starsmedia.com>
2015-05-09 12:50:29 +0200

    added notes

---
+++ web/02embedpages.php
@@ -0,0 +1,66 @@
+<?php
+
+echo <<<'HTMLCODE'
+
+<html>
+<head>
+</head>
+<body>
+<h1>Embeds</h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4Q6Nurtb_9c" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OV8BvxpNQOs" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qq2NYKBwIqk" frameborder="0" allowfullscreen></iframe></br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2I6fuD20qlY" frameborder="0" allowfullscreen></iframe></br>
+
+</body>
+</html>
+
+HTMLCODE;
+
+
+
+?>
---
+++ web/shscript-notes.sh
@@ -0,0 +1,5 @@
+for i in {1..3}
+do
+   echo "Test number $i times"
+   vendor/bin/phpunit WebDriverDemo_Yumpu1.php 
+done
